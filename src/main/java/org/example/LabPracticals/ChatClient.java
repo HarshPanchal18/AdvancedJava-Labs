3b646594f69f0ee8fa98b34921a4c02d5c5258fb
Harsh Panchal <harshhhh1803@gmail.com>
2024-08-26 15:21:03 +0530

    Implementing Dual chat

--- src/main/java/org/example/LabPracticals/ChatClient.java
+++ src/main/java/org/example/LabPracticals/ChatClient.java
@@ -5,18 +5,30 @@ import java.net.Socket;
 
 public class ChatClient {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 6000);
+        Socket socket = new Socket("localhost", 5555);
+//        Socket socket = new Socket("192.168.137.145", 5555);
         System.out.println("Client is online...");
 
+        DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String message;
+        String sentMessage;
+        String receivedMessage;
         do {
-            message = br.readLine();
-            out.writeUTF(message);
-            System.out.println(message);
-        } while (!message.equalsIgnoreCase("stop"));
+            System.out.print("Your message: ");
+
+            sentMessage = br.readLine();
+            out.writeUTF(sentMessage);
+
+            System.out.println("Client: " + sentMessage);
+            System.out.println();
+
+            receivedMessage = in.readUTF();
+            System.out.println("Server: " + receivedMessage);
+            System.out.println();
+
+        } while (!sentMessage.equalsIgnoreCase("stop"));
 
         System.out.println("Terminated connection...");
 
--- src/main/java/org/example/LabPracticals/ChatServer.java
+++ src/main/java/org/example/LabPracticals/ChatServer.java
@@ -1,26 +1,36 @@
 package org.example.LabPracticals;
 
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(6000);
+        ServerSocket server = new ServerSocket(5555);
         System.out.println("Server is online...");
 
         Socket socket = server.accept();
         System.out.println("Client is connected...");
 
         DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        BufferedReader ibr = new BufferedReader(new InputStreamReader(System.in));
 
-        String message;
+        String receivedMessage;
+        String sentMessage;
         try {
             do {
-                message = in.readUTF();
-                System.out.println(message);
-            } while (!message.equalsIgnoreCase("stop"));
+                receivedMessage = in.readUTF();
+                System.out.println("Client: " + receivedMessage);
+
+                System.out.print("Your message: ");
+                sentMessage = ibr.readLine();
+                out.writeUTF(sentMessage);
+
+                System.out.println("Server: " + sentMessage);
+                System.out.println();
+
+            } while (!receivedMessage.equalsIgnoreCase("stop"));
         } catch (Exception ignored) {
         }
 
